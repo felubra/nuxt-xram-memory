47774ac24c78b396e774a22af78b84d517508da7
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-06 18:12:23 -0200

    layout para a página inicial

---
+++ src/layouts/homepage.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-container>
+    <el-header height="250px">
+      <el-row 
+        type="flex" 
+        justify="space-around">
+        <el-col 
+          :span="5" 
+          class="logo">
+          <xram-memory-logo/>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <nuxt/>
+    </el-main>
+    <el-footer>
+      <el-row 
+        type="flex" 
+        justify="space-around">
+        <el-col :span="24">
+          <xram-memory-footer/>
+        </el-col>
+      </el-row>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+import XramMemoryLogo from '@/components/xram-memory/logo.vue'
+export default {
+  components: {
+    XramMemoryLogo
+  }
+}
+</script>
+
+
+<style scoped>
+div.logo {
+  padding-top: 150px;
+  text-align: center;
+}
+</style>
