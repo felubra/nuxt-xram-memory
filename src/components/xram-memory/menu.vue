0b5e452c29508ebb020402ea76d052aff0b09922
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-06 18:11:39 -0200

    esqueleto do componente para o menu do site

---
+++ src/components/xram-memory/menu.vue
@@ -0,0 +1,15 @@
+<script>
+/**
+ * Menu com a navegação principal do site.
+ *
+ * - Requisitos:
+ *  - TODO: Itens fixos
+ */
+export default {
+  name: 'XramMemoryMenu'
+}
+</script>
+
+<template>
+  <nav />
+</template>
