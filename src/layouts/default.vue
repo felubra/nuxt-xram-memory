d67ae630710771fb6fb7457d58f30cdedaf96104
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-06 18:12:11 -0200

    layout padrão

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,45 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <el-container>
+    <el-header>
+      <el-row 
+        type="flex" 
+        justify="center">
+        <el-col 
+          :span="3" 
+          class="logo">
+          <xram-memory-logo/>
+        </el-col>
+        <el-col :span="15">
+          <xram-memory-menu/>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <nuxt/>
+    </el-main>
+    <el-footer>
+      <el-row 
+        type="flex" 
+        justify="space-around">
+        <el-col :span="24">
+          <xram-memory-footer/>
+        </el-col>
+      </el-row>
+    </el-footer>
+  </el-container>
 </template>
+
+<script>
+import XramMemoryLogo from '@/components/xram-memory/logo.vue'
+import XramMemoryMenu from '@/components/xram-memory/menu.vue'
+export default {
+  components: {
+    XramMemoryLogo,
+    XramMemoryMenu
+  }
+}
+</script>
+
 
 <style>
 </style>
