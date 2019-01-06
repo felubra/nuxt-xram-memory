e0cddd5bd5bdc2abf177035cec0ac11885b1480d
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-06 18:12:44 -0200

    página inicial

---
+++ src/pages/index.vue
@@ -0,0 +1,40 @@
+<script>
+/**
+ * Página inicial
+ *
+ * - Requisitos:
+ *  - Uma entrada para a busca do site
+ *  - Listas de coleções em destaque
+ */
+import XramMemorySearchBar from '@/components/xram-memory/search-bar.vue'
+export default {
+  layout: 'homepage',
+  components: {
+    XramMemorySearchBar
+  }
+}
+</script>
+
+<template>
+  <div>
+    <el-row 
+      type="flex"
+      justify="space-around">
+      <el-col :span="7">
+        <xram-memory-search-bar/>
+      </el-col>
+    </el-row>
+    <el-row
+      v-for="featured_collection in featured_collections"
+      :key="featured_collection.id"
+      type="flex"
+      justify="space-around"
+    >
+      <el-col :span="18">
+        <xram-memory-collection 
+          :mode="showcase" 
+          :collection="featured_collection"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
