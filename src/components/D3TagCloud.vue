1283ab9b1f88a9b41d62989cf30f781ea8e17c82
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-12 17:34:55 -0200

    feat (cloud): organização do código e responsividade - A nuvem responde ao evento resize de window

--- src/components/D3TagCloud.vue
+++ src/components/D3TagCloud.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="sky"></div>
+  <div class="sky" ref="sky"></div>
 </template>
 
 <script>
@@ -15,12 +15,99 @@ export default {
       type: Array
     }
   },
+  methods: {
+    makeCloud(event) {
+      const el = this.$refs.sky
+      return this.$nextTick(function() {
+        const dimensions = [Math.min(el.clientWidth, 1200), el.clientHeight]
+
+        this.layout = d3Cloud()
+          .size(dimensions)
+          .words(this.words)
+          .padding(2)
+          .rotate(function(d) {
+            const avaliableAngles = [15, 45, 60, 90]
+            const angle =
+              avaliableAngles[
+                Math.floor(Math.random() * avaliableAngles.length)
+              ]
+            const avNums = [2, 3, 5, 9]
+            const numSteps = avNums[Math.floor(Math.random() * avNums.length)]
+            const step = (angle * 2) / numSteps
+            const angles = Array.apply(undefined, Array(numSteps + 1)).map(
+              (n, i, c) => {
+                return i * step - angle
+              }
+            )
+            return angles[Math.floor(Math.random() * angles.length)]
+          })
+          .font('Asmath Solid')
+          .fontSize(function(d) {
+            return d.size
+          })
+          .on('end', this.drawCloud)
+        this.layout.start()
+      })
+    },
+    drawCloud(words) {
+      this.$nextTick(function() {
+        try {
+          const svgEl = this.$refs.sky.querySelector('svg')
+          if (svgEl) {
+            this.$refs.sky.removeChild(svgEl)
+          }
+        } catch {
+          //TODO: verificar porque o elemento this.$refs.sky não está disponível
+        }
+        d3.select(this.$refs.sky)
+          .append('svg')
+          .attr('width', this.layout.size()[0])
+          .attr('height', this.layout.size()[1])
+          .append('g')
+          .attr(
+            'transform',
+            'translate(' +
+              this.layout.size()[0] / 2 +
+              ',' +
+              this.layout.size()[1] / 2 +
+              ')'
+          )
+          .selectAll('text')
+          .data(words)
+          .enter()
+          .append('text')
+          .style('font-size', function(d) {
+            return d.size + 'px'
+          })
+          .style('fill', function(d) {
+            const color = chromaJS.scale(['#AA0000', '#f00'])(
+              (d.size - 1) / (100 - 1)
+            )
+            return color
+          })
+          .style('font-family', 'Asmath Solid')
+          .attr('text-anchor', 'middle')
+          .attr('transform', function(d) {
+            return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')'
+          })
+          .append('a')
+          .attr('href', function(d) {
+            return '/keyword/' + slugify(d.text)
+          })
+          .text(function(d) {
+            return d.text
+          })
+      })
+    }
+  },
   computed: {
     words() {
+      //TODO: calcular isso daqui em função da largura do container
+      const FONT_SIZE_DELTA = 16
       return this.keywords.map(keyword => {
         return {
           text: keyword.key,
-          size: (keyword.doc_count / this.wordsStdDeviation) * 14
+          size: (keyword.doc_count / this.wordsStdDeviation) * FONT_SIZE_DELTA
         }
       })
     },
@@ -51,68 +138,27 @@ export default {
       return standardDeviation(this.keywords.map(keyword => keyword.doc_count))
     }
   },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.makeCloud)
+  },
+  created() {
+    this.layout = {}
+  },
   mounted() {
-    const sky = this.$refs.sky
-    function draw(words) {
-      d3.select(sky)
-        .append('svg')
-        .attr('width', layout.size()[0])
-        .attr('height', layout.size()[1])
-        .append('g')
-        .attr(
-          'transform',
-          'translate(' + layout.size()[0] / 2 + ',' + layout.size()[1] / 2 + ')'
-        )
-        .selectAll('text')
-        .data(words)
-        .enter()
-        .append('text')
-        .style('font-size', function(d) {
-          return d.size + 'px'
-        })
-        .style('fill', function(d) {
-          const color = chromaJS.scale(['#AA0000', '#f00'])(
-            (d.size - 1) / (100 - 1)
-          )
-          return color
-        })
-        .style('font-family', 'Asmath Solid')
-        .attr('text-anchor', 'middle')
-        .attr('transform', function(d) {
-          return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')'
-        })
-        .append('a')
-        .attr('href', function(d) {
-          return '/keyword/' + slugify(d.text)
-        })
-        .text(function(d) {
-          return d.text
-        })
-    }
-    const layout = d3Cloud()
-      .size([960, 500])
-      .words(this.words)
-      .padding(2)
-      .rotate(function(d) {
-        const avaliableAngles = [15, 45, 60, 90]
-        const angle =
-          avaliableAngles[Math.floor(Math.random() * avaliableAngles.length)]
-        const avNums = [2, 3, 5, 9]
-        const numSteps = avNums[Math.floor(Math.random() * avNums.length)]
-        const step = (angle * 2) / numSteps
-        const angles = Array.apply(undefined, Array(numSteps + 1)).map(
-          (n, i, c) => {
-            return i * step - angle
-          }
-        )
-        return angles[Math.floor(Math.random() * angles.length)]
-      })
-      .font('Asmath Solid')
-      .fontSize(function(d) {
-        return d.size
-      })
-      .on('end', draw)
-    layout.start()
+    this.makeCloud(this.$refs.sky)
+    window.addEventListener('resize', this.makeCloud)
+  },
+  updated() {
+    this.makeCloud(this.$refs.sky)
+    window.addEventListener('resize', this.makeCloud)
   }
 }
 </script>
+
+<style>
+div.sky {
+  position: relative;
+  width: 100%;
+  min-height: 600px;
+}
+</style>
