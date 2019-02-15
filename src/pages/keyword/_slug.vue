ddabd5ad1b0f8be5f16a75955d0823ff17647dd7
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-15 16:36:36 -0200

    feat: utilize o novo componente NewsCard

--- src/pages/keyword/_slug.vue
+++ src/pages/keyword/_slug.vue
@@ -12,7 +12,7 @@
         :size="5"
       >
       <div slot="renderData" slot-scope="{ item }">
-        <h1>{{item.title}}</h1>
+        <NewsCard :newsItem="item"></NewsCard>
       </div>
       </ReactiveList>
     
@@ -20,7 +20,9 @@
 </template>
 
 <script>
-import ReactiveTagCloud from '~/components/ReactiveTagCloud'
+//TODO: inserir o título da palavra-chave
+import NewsCard from '~/components/NewsCard'
+
 export default {
   name: 'slugPage',
   computed: {
@@ -45,41 +47,12 @@ export default {
         };
     },
   },
+  components: {
+    NewsCard
+  }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-.searchBar {
-  margin: 10px;
-}
 </style>
