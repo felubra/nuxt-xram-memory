71fb13b944dea245ae5bbd27e76ed6bdc72f26cb
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-17 11:11:01 -0300

    feat(design): NewsCard: melhorias no design - exiba o texto truncado do teaser a partir de certa largura - defina ma largura máxima de 850px - corrija a margem inferior do título

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -2,7 +2,10 @@
   <div class="news-card">
     <div class="news-body">
       <img v-if="image" :src="image" alt="">
-      <h3>{{ newsItem.title }}</h3>
+      <div class="news-text">
+        <h3>{{ newsItem.title }}</h3>
+        <p class="teaser">{{teaser}}</p>
+      </div>
     </div>
     <div class="news-footer">
       <p><strong>O Estado de São Paulo</strong> 14/02/2019</p>
@@ -11,6 +14,8 @@
 </template>
 
 <script>
+const smartTruncate = require('smart-truncate')
+
 export default {
   name: 'NewsCard',
   props: {
@@ -26,7 +31,7 @@ export default {
       return this.newsItem.title
     },
     teaser() {
-      return this.newsItem.teaser
+      return smartTruncate(this.newsItem.teaser, 300)
     },
     image() {
       if (this.newsItem.image_capture) {
@@ -46,11 +51,16 @@ div.news-card {
   margin: 1.2rem 0;
   display: flex;
   flex-direction: column;
+  text-align: center;
+  max-width: 850px;
 }
 
-div.news-card > div.news-body {
+div.news-body {
   justify-content: flex-start;
-  text-align: center;
+}
+
+div.news-body p.teaser {
+  display: none;
 }
 
 div.news-body img {
@@ -59,10 +69,11 @@ div.news-body img {
 
 div.news-body h3 {
   font-weight: 500;
-  margin: 0 0.5rem 1.7rem;
+  margin: 0 0.5rem 1rem;
   font-size: 1.25rem;
   line-height: 1.2;
 }
+
 div.news-footer {
   justify-content: flex-end;
 }
@@ -72,6 +83,31 @@ div.news-footer p {
   font-size: 0.75rem;
   margin: 0;
   padding: 0;
-  text-align: center;
+}
+@media only screen and (min-width: 768px) {
+  div.news-card {
+    text-align: left;
+  }
+  div.news-body {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    position: relative;
+    text-align: left;
+  }
+
+  div.news-body img {
+    align-self: baseline;
+  }
+
+  div.news-footer p {
+    font-size: 0.8rem;
+  }
+
+  div.news-body p.teaser {
+    display: block;
+    text-align: justify;
+    margin: 0 0.5rem 1.7rem;
+  }
 }
 </style>
