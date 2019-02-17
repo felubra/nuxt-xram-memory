c464f63ddb2a8edc09630a031886ec2c673dd5e2
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-17 11:31:35 -0300

    feat(design): layout padrão: defina o tema de ReactiveBase e paddings

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,6 +6,33 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'DefaultLayout',
+  computed: {
+    defaultTheme() {
+      return {
+        typography: {
+          fontFamily: 'Vollkorn, serif',
+          fontSize: '16px'
+        },
+
+        colors: {
+          textColor: '#333333',
+          backgroundColor: '#fff',
+          primaryTextColor: '#AA0000',
+          primaryColor: '#2196F3',
+          titleColor: '#fff',
+          alertColor: '#d9534f',
+          borderColor: '#666'
+        }
+      }
+    }
+  }
+}
+</script>
+
+
 <style>
 html {
   font-family: 'Vollkorn', serif;
@@ -16,5 +43,16 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+@media only screen and (min-width: 410px) {
+  body {
+    padding: 1rem;
+  }
 }
 </style>
