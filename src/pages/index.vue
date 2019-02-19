43463b5d9cba82f0f795e359618853f1fd496313
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-19 18:10:16 -0300

    feat: página com resultados de pesquisa dedicada

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,52 +1,21 @@
 <template>
   <section class="container">
+    <div class="welcome-message">
+      <p><em>O </em><strong>Xram-Memory</strong><em> tem o objetivo de manter a memória dos assuntos que a esquerda quer esquecer. </em><a href="/sobre" class="read-more microtext">Saiba mais</a></p>
+    </div>
     <DataSearch
       class="result-list-container"
-      category-field="authors"
       component-id="SearchSensor"
-      :field-weights="[10,7,1]"
+      :field-weights="[10,7]"
       :data-field="['title', 'teaser']"
       icon-position="right"
       :autosuggest="false"
       class-name="searchBar"
       placeholder="Pesquisar no acervo"
       :show-clear="false"
+      :u-r-l-params="true"
+      @keyPress="search"
     />
-    <div class="filters">
-      <single-dropdown-list 
-        :default-query="customFilterQuery"
-        filter-label="Site/Veículo"
-        component-id="NewspaperSensor"
-        data-field="newspaper.title"
-        nested-field="newspaper"
-        class-name="filterItem"
-        placeholder="Todos"
-        :inner-class="{
-          title: 'microtext'
-        }"
-        title="Site/Veículo"
-      />
-    </div>
-
-    <div class="results">
-      <ReactiveList
-        :react="{and: ['SearchSensor','NewspaperSensor']}"
-        component-id="SearchResult"
-        :pagination="false"
-        data-field="title.raw"
-        class-name="result-list-container"
-        :inner-class="{
-          resultsInfo: 'results-info'
-        }"
-        :from="0"
-        :size="5"
-      >
-        <div slot="renderData" slot-scope="{ item }">
-          <NewsCard class="news-card" :news-item="item" />
-        </div>
-
-      </ReactiveList>
-    </div>
     <no-ssr>
       <ReactiveComponent component-id="ReactiveD3TagCloud" :default-query="tagCloudQuery" >
         <div slot-scope="{ aggregations, error }">
@@ -59,12 +28,11 @@
 
 <script>
 import ReactiveD3TagCloud from '~/components/ReactiveD3TagCloud'
-import NewsCard from '~/components/NewsCard'
+
 import { TAGCLOUD_QUERY } from '~/config/constants'
 export default {
   components: {
-    ReactiveD3TagCloud,
-    NewsCard
+    ReactiveD3TagCloud
   },
   computed: {
     tagCloudQuery() {
@@ -74,74 +42,37 @@ export default {
     }
   },
   methods: {
-    /**
-     * Aparentemente a query montada pelo ReactiveSearch não funciona, então usemos esta.
-     */
-    customFilterQuery(_, { nestedField, dataField }) {
-      return {
-        query: {
-          match_all: {}
-        },
-        size: 0,
-        aggs: {
-          reactivesearch_nested: {
-            nested: {
-              path: nestedField
-            },
-            aggs: {
-              [dataField]: {
-                terms: {
-                  field: dataField,
-                  size: '10',
-                  order: {
-                    _count: 'desc'
-                  }
-                }
-              }
-            }
-          }
-        }
+    search(e) {
+      if (event.which == 13 || event.keyCode == 13) {
+        this.$router.push({
+          name: 'search-query',
+          params: { query: e.target.value }
+        })
       }
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
 .searchBar {
-  margin: 10px;
-}
-.results {
-  background: #e0e0e0;
-}
-.results-info {
-  color: #555555;
-  text-transform: uppercase;
-  font-family: 'Cabin', sans-serif;
-  margin-left: 12px;
-}
-.news-card {
-  margin: 0.75rem;
-}
-.filters {
-  background: #f3f1f1;
-  padding: 12px;
-}
-.filterItem button {
-  border: none;
-  border-bottom: solid 1px #a1a1a1;
-  background: transparent;
-  font-family: 'Volkorn', serif;
-  font-size: 1rem;
-  font-style: italic;
+  margin: 25vh 1rem 15vh;
 }
 
-.filterItem ul > li {
-  font-family: 'Volkorn', serif;
-  font-size: 1rem;
+.welcome-message {
+  font-size: 1.375rem;
+  text-align: center;
+  margin: 1rem;
 }
 
-.filterItem h2 {
-  font-size: 0.875rem;
+.welcome-message a {
+  display: block;
+  text-align: right;
+  font-size: 0.75rem;
 }
 </style>
---
+++ src/pages/search/_query.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <div class="results">
+      <DataSearch
+        class="result-list-container"
+        component-id="SearchSensor"
+        :field-weights="[10,7]"
+        :data-field="['title', 'teaser']"
+        icon-position="right"
+        :autosuggest="false"
+        class-name="searchBar"
+        placeholder="Pesquisar no acervo"
+        :show-clear="false"
+        :default-selected="query"
+      />
+      <ReactiveList
+        :react="{and: ['SearchSensor','NewspaperSensor']}"
+        component-id="SearchResult"
+        :pagination="false"
+        data-field="title.raw"
+        class-name="result-list-container"
+        :inner-class="{
+          resultsInfo: 'results-info'
+        }"
+        :from="0"
+        :size="5"
+      >
+        <div slot="renderData" slot-scope="{ item }">
+          <NewsCard class="news-card" :news-item="item"/>
+        </div>
+      </ReactiveList>
+      <div class="filters">
+        <single-dropdown-list
+          :default-query="customFilterQuery"
+          filter-label="Site/Veículo"
+          component-id="NewspaperSensor"
+          data-field="newspaper.title"
+          nested-field="newspaper"
+          class-name="filterItem"
+          placeholder="Todos"
+          :inner-class="{
+            title: 'microtext'
+          }"
+          title="Site/Veículo"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewsCard from '~/components/NewsCard'
+
+export default {
+  name: 'SearchPage',
+  components: {
+    NewsCard
+  },
+  computed: {
+    query() {
+      try {
+        return this.$route.params.query
+      } catch {
+        return ''
+      }
+    }
+  },
+  methods: {
+    /**
+     * Aparentemente a query montada pelo ReactiveSearch não funciona, então usemos esta.
+     */
+    customFilterQuery(_, { nestedField, dataField }) {
+      return {
+        query: {
+          match_all: {}
+        },
+        size: 0,
+        aggs: {
+          reactivesearch_nested: {
+            nested: {
+              path: nestedField
+            },
+            aggs: {
+              [dataField]: {
+                terms: {
+                  field: dataField,
+                  size: '10',
+                  order: {
+                    _count: 'desc'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.results {
+  background: #e0e0e0;
+}
+.results-info {
+  color: #555555;
+  text-transform: uppercase;
+  font-family: 'Cabin', sans-serif;
+  margin-left: 12px;
+}
+.news-card {
+  margin: 0.75rem;
+}
+.filters {
+  background: #f3f1f1;
+  padding: 12px;
+}
+.filterItem button {
+  border: none;
+  border-bottom: solid 1px #a1a1a1;
+  background: transparent;
+  font-family: 'Volkorn', serif;
+  font-size: 1rem;
+  font-style: italic;
+}
+
+.filterItem ul > li {
+  font-family: 'Volkorn', serif;
+  font-size: 1rem;
+}
+
+.filterItem h2 {
+  font-size: 0.875rem;
+}
+</style>
