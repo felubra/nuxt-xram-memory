2ef94df9abb3648295a56f4f0fb5ebe98309e9d7
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-22 16:36:29 -0300

    feat: mais propriedades usadas pelos componentes de visualização

--- src/components/DocumentPreview.vue
+++ src/components/DocumentPreview.vue
@@ -1,4 +1,6 @@
 <script>
+const humanSize = require('human-size')
+const dayJs = require('dayjs')
 export default {
   name: 'DocumentPreview',
   props: {
@@ -9,10 +11,35 @@ export default {
   },
   computed: {
     file_url() {
+      /** TODO: adicionar prefixo */
       return this.docItem.url
     },
     title() {
+      /** TODO: sanitizar */
       return this.docItem.title
+    },
+    description() {
+      /** TODO: sanitizar */
+      return this.docItem.teaser
+    },
+    size() {
+      try {
+        return humanSize(this.docItem.file_size)
+      } catch {
+        return '0KB'
+      }
+    },
+    sendDate() {
+      try {
+        const dateTime = dayJs(this.docItem.modified_at)
+        if (!dateTime.isValid()) {
+          throw new Error()
+        }
+        const localDateTime = dateTime.toDate().toLocaleDateString()
+        return `Enviado em ${localDateTime}`
+      } catch {
+        return ''
+      }
     }
   }
 }
