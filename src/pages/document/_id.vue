15f84149a59f31edbdb4e61ad2cf709f31371161
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-25 15:51:26 -0300

    feat: adicione mais documentos, refatoração de funções

--- src/pages/document/_id.vue
+++ src/pages/document/_id.vue
@@ -12,27 +12,48 @@ export default {
   },
   computed: {
     documentId() {
-      return this.$route.params.id || null
+      return parseInt(this.$route.params.id) || null
+    },
+    documents() {
+      return [
+        {
+          id: 1,
+          title: 'Ejud.zip',
+          teaser: 'Arquivos do processo de Lula',
+          file_size: 5 * 1000 * 1000,
+          url: '/media/abacate.txt',
+          createdAt: '2008-09-15T15:53:00+05:00',
+          modifiedAt: '2008-09-15T15:53:00+05:00'
+        },
+        {
+          id: 2,
+          title: 'Filomena.jpeg',
+          teaser: 'Um texto muito bonito',
+          mime_type: 'image/jpg',
+          file_size: 5 * 666 * 1000,
+          url:
+            '/media/artifacts/documents/image_files/_2019-01-29_14.28.51.0854001546379483822.jpg',
+          createdAt: '2008-09-15T15:53:00+05:00',
+          modifiedAt: '2008-09-15T15:53:00+05:00'
+        }
+      ]
     },
     document() {
-      return {
-        title: 'Ejud.zip',
-        teaser: 'Arquivos do processo de Lula',
-        file_size: 5 * 1000 * 1000,
-        url: '/media/abacate.txt',
-        createdAt: '2008-09-15T15:53:00+05:00',
-        modifiedAt: '2008-09-15T15:53:00+05:00'
-      }
+      return this.documents.find(document => document.id === this.documentId)
     },
     previewComponentType() {
-      if (this.document.mime_type) {
-        if (this.document.mime_type === 'application/pdf') {
-          return 'PDFFilePreview'
-        } else if (this.document.mime_type.contains('image/')) {
-          return 'ImageFilePreview'
-        } else {
-          return 'UnknownFilePreview'
+      try {
+        if (this.document.mime_type) {
+          if (this.document.mime_type === 'application/pdf') {
+            return 'PDFFilePreview'
+          } else if (this.document.mime_type.includes('image/')) {
+            return 'ImageFilePreview'
+          } else {
+            return 'UnknownFilePreview'
+          }
         }
+      } catch {
+        return 'UnknownFilePreview'
       }
       return 'UnknownFilePreview'
     },
