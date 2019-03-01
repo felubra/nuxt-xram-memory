088168fd59b9c61874f1dab3cada786984a7223d
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-03-01 12:56:13 -0300

    fix: obtenha a notícia da api pelo id, não pelo slug

---
+++ src/pages/news/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="Page">
+    <NewsCard :label="true" :footer="false" :news-item="newsItem"/>
+    <NewsInfo :news-item="newsItem"/>
+  </section>
+</template>
+
+<script>
+import NewsCard from '~/components/news/NewsCard'
+import NewsInfo from '~/components/news/NewsInfo'
+export default {
+  components: {
+    NewsCard,
+    NewsInfo
+  },
+  data() {
+    return {
+      newsItem: {}
+    }
+  },
+  async asyncData({ $axios, route }) {
+    const newsId = parseInt(route.params.id) || null
+    return $axios.$get(`/api/v1/news/${newsId}`).then(newsItem => {
+      return { newsItem }
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>
