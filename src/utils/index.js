e830b8bfddcb23f4cf812f9977848cea4067bce4
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-03-03 19:37:20 -0300

    feat: filtro xss com suporte à classes do editor quill

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+const xss = require('xss')
 export const getMediaUrl = function(path) {
   try {
     return `${process.env.mediaUrl}${path}`
@@ -5,3 +6,64 @@ export const getMediaUrl = function(path) {
     return path
   }
 }
+
+const defaultWhiteList = {
+  section: ['id', 'class'],
+  div: ['class'],
+
+  h1: [],
+  h2: [],
+  h3: [],
+  h4: [],
+  h5: [],
+  h6: [],
+
+  img: ['src'],
+  small: [],
+
+  b: [],
+  em: [],
+  p: ['class'],
+  hr: [],
+  address: [],
+  a: ['target', 'class', 'href'],
+  span: [],
+  strong: []
+}
+
+const xssFilterFactory = (allowedClasses = null, whiteList = null) => {
+  const wl = whiteList || defaultWhiteList
+
+  return {
+    whiteList: wl,
+    stripIgnoreTag: true,
+    stripIgnoreTagBody: true,
+    onTagAttr(tag, name, value, isWhiteAttr) {
+      // Permita somente classes em allowedClasses
+      if (isWhiteAttr && name === 'class') {
+        let classes = value.split(' ')
+        if (typeof allowedClasses === 'function') {
+          classes = allowedClasses(classes).join(' ')
+        } else if (Array.isArray(allowedClasses)) {
+          classes = classes
+            .filter(className => allowedClasses.includes(className))
+            .join(' ')
+        }
+        return xss.onTagAttr(tag, name, classes, isWhiteAttr)
+      }
+      // Adicione o endereço da api em atributos src relativos de tags img
+      if (tag === 'img' && name === 'src') {
+        if (!value.match(/^https?:\/\//)) {
+          return `src=${xss.escapeAttrValue(process.env.API_URL + value)}`
+        }
+      }
+      return xss.onTagAttr(tag, name, value, isWhiteAttr)
+    }
+  }
+}
+
+/** Permita classes que são prefixadas com ql- (do Quill Editor) */
+const allowQuillClasses = classes =>
+  classes.filter(klass => klass.startsWith('ql-'))
+
+export const appClassesXSSFilter = xssFilterFactory(allowQuillClasses)
