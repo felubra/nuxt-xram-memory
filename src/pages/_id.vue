c50975bcc0cf41345d28ad5bb661e5e1dc0462c5
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-03-07 15:44:19 -0300

    feat: página para páginas estáticas

---
+++ src/pages/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="Page">
+    <StaticPage :page-item="staticPage"/>
+  </section>
+</template>
+<script>
+import StaticPage from '~/components/common/StaticPage'
+export default {
+  components: {
+    StaticPage
+  },
+  data() {
+    return { staticPage: {} }
+  },
+  async asyncData({ $axios, route }) {
+    const id = parseInt(route.params.id) || null
+    return $axios.$get(`/api/v1/pages/${id}`).then(staticPage => {
+      return { staticPage }
+    })
+  }
+}
+</script>
