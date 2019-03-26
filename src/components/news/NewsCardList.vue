28d9cdf55cdf80891dece9f50447a0e6f71d4928
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-03-26 18:45:37 -0300

    feat: componente para lista de notícias

---
+++ src/components/news/NewsCardList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      <NewsCard :news-item="item"></NewsCard>
+    </li>
+  </ul>
+</template>
+<script>
+import NewsCard from './NewsCard'
+export default {
+  name: 'NewsCardList',
+  components: {
+    NewsCard
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: block;
+  max-width: 70rem;
+}
+
+li {
+  margin: 1rem;
+}
+</style>
