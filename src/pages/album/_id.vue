f1941c2ddc4eb0579dae57ce111c289f007b9dd2
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-04-12 17:45:11 -0300

    feat(wip): página do álbum

---
+++ src/pages/album/_id.vue
@@ -0,0 +1,175 @@
+<template>
+  <AbstractPage class="Page--sidebar Page--aside-after">
+    <template v-slot:aside>
+      <div class="PageAside AlbumInfo">
+        <p class="microtext">Album de fotos</p>
+        <h1>{{album.name}}</h1>
+      </div>
+    </template>
+    <!--<ImageFilePreview :fileURL="selectedFileURL"></ImageFilePreview>-->
+    <no-ssr>
+      <resize-sensor @resize="selectImageSize"></resize-sensor>
+    </no-ssr>
+  </AbstractPage>
+</template>
+<script>
+import ImageFilePreview from '~/components/viewers/ImageFilePreview'
+import AbstractPage from '~/components/common/AbstractPage'
+
+const { getMediaUrl, sanitize } = require('~/utils')
+export default {
+  components: {
+    AbstractPage,
+    ImageFilePreview
+  },
+  data() {
+    return { album: {}, selectedPhoto: {}, selectedImageSize: null }
+  },
+  head() {
+    return {
+      title: this.album.name,
+      titleTemplate: 'xraM-Memory - Álbum de fotos: %s'
+    }
+  },
+  computed: {
+    availableSizes() {
+      return Object.keys(this.selectedPhoto.thumbnails)
+        .filter(thumbnailName => parseInt(thumbnailName, 10))
+        .map(size => parseInt(size, 10))
+    },
+    failbackSize() {
+      return Math.min(...this.availableSizes)
+    }
+  },
+  methods: {
+    selectImageSize({ width, height }) {
+      const largestSide = Math.max(width, height)
+      /**
+       * com base nas dimensões calculadas, escolha o maior tamanho disponível que caberá nessas dimensões
+       */
+    }
+  },
+  async asyncData({ $axios, route, redirect, error }) {
+    const albumId = parseInt(route.params.id, 10) || null
+    if (!!albumId) {
+      return $axios.$get(`/api/v1/album/${albumId}`).then(album => {
+        const selectedPhoto = album.photos[0]
+        return { album, selectedPhoto }
+      })
+    } else {
+      error({ statusCode: 404, message: 'Álbum não encontrado' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+  font-size: 1rem;
+  word-break: break-all;
+  margin: 0;
+}
+
+p.microtext + h1 {
+  margin: 0;
+}
+
+.FilePreview__Label {
+  color: inherit;
+  font-size: 0.75rem;
+  margin-top: 0;
+}
+.FilePreview__Description {
+  margin: 0;
+  font-size: 1rem;
+}
+
+.FileInfo__Description {
+  max-height: 60vh;
+  overflow-y: auto;
+  color: #333;
+  text-align: justify;
+}
+
+dl {
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+dl > div {
+  margin: 0.5rem 1rem;
+  margin-left: 0;
+}
+
+dt {
+  margin: 0.5rem 0 0 0;
+}
+dd {
+  margin: 0;
+  color: #333;
+}
+
+.FileActions {
+  display: flex;
+  margin: auto 0.5rem;
+  padding: 1rem 0;
+  justify-content: center;
+}
+
+.FileActions > a {
+  display: inline-block;
+  text-decoration: none;
+  text-align: center;
+  color: #333;
+}
+
+.FileActions > a:hover,
+.FileActions > a:focus,
+.FileActions > a:active {
+  color: #ce5454;
+}
+
+.FileInfo_Button > i {
+  display: block;
+  font-size: 48px;
+}
+@media only screen and (min-width: 768px) {
+  dl {
+    justify-content: flex-start;
+  }
+
+  .FileActions {
+    display: flex;
+    padding: 0;
+    justify-content: center;
+    position: fixed;
+    backface-visibility: hidden;
+    z-index: 100;
+    bottom: 1rem;
+    right: 2.5rem;
+  }
+
+  .FileActions a {
+    opacity: 0.8;
+    margin: 0.5rem;
+    text-transform: uppercase;
+  }
+
+  .FileActions a:hover,
+  .FileActions a:focus,
+  .FileActions a:active {
+    opacity: 1;
+  }
+
+  dl {
+    margin-top: auto;
+  }
+  dl > div:first-of-type {
+    margin-left: 0;
+  }
+}
+</style>
+
