2b8df377bc1c3030c5e4fc2a2e77f28bbd439ce5
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-04-22 21:52:21 -0300

    fix: remoção de configuração da url do ES

--- src/utils/reactiveMixin.js
+++ src/utils/reactiveMixin.js
@@ -1,11 +1,12 @@
-import { defaultTheme, ELASTIC_SEARCH_SERVER } from '~/config/constants'
+import { defaultTheme } from '~/config/constants'
+
 export default {
   computed: {
     reactiveDefaultTheme() {
       return defaultTheme
     },
     reactiveServerURL() {
-      return ELASTIC_SEARCH_SERVER;
+      return process.env.ELASTIC_SEARCH_SERVER
     }
   }
 }
