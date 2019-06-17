dcbd8df96a86d50cbb64ac7da1faf39d0586e1c0
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-06-17 10:36:08 -0300

    feat: componente de rodapé

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>&copy; 2019 xRAM-Memory</p>
+    <p class="Footer_Link">
+      <a>Temos de uso</a>
+      <a>Política de privacidade</a>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+footer {
+  text-align: center;
+}
+</style>
