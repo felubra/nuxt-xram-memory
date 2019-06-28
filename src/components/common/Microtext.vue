0f97b5001aa25d1428255d4030a77da4bfb644ed
Felipe Lube de Bragança <felipe.braganca@PRODEST.ES.GOV.BR.LOCAL>
2019-06-28 18:25:00 -0300

    feat: [WIP] componente de microtexto

---
+++ src/components/common/Microtext.vue
@@ -0,0 +1,36 @@
+<template functional>
+  <p>
+    <slot></slot>&nbsp;
+    <i v-if="hasArrow" class="material-icons">arrow_bottom</i>
+  </p>
+</template>
+<script>
+export default {
+  name: 'Microtext',
+  props: {
+    arrow: {
+      type: String,
+      validator(value) {
+        return ['right', 'top', 'bottom', 'left', 'none'].includes(value)
+      },
+      default: 'none'
+    }
+  },
+  computed: {
+    hasArrow() {
+      return this.arrow !== 'none'
+    }
+  }
+}
+</script>
+<style scoped>
+p {
+  font-family: 'Cabin', sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 0.875;
+  line-height: 1.06rem;
+  text-transform: uppercase;
+  color: #d84848;
+}
+</style>
