ce34619a441221eb7491f8ac2a50a2a94f67ebb6
Felipe Lube de Bragança <felipe.braganca@PRODEST.ES.GOV.BR.LOCAL>
2019-06-28 18:24:21 -0300

    feat: [WIP] componente de rodapé

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,19 +1,52 @@
 <template>
-  <footer>
-    <p>&copy; 2019 xRAM-Memory</p>
-    <p class="Footer_Link">
-      <a>Temos de uso</a>
-      <a>Política de privacidade</a>
+  <footer class="MainFooter">
+    <Logo></Logo>
+    <p>
+      &copy; 2019 xRAM-Memory.
+      <br>Todos os direitos reservados.
     </p>
+    <nuxt-link v-for="(index, link) in footerLinks" :key="index">{{link.name}}</nuxt-link>
   </footer>
 </template>
 
 <script>
-export default {}
+import Logo from './Logo'
+import { mapGetters } from 'vuex'
+export default {
+  components: {
+    Logo
+  },
+  computed: {
+    footerLinks() {
+      return this.pageLinks('menu-footer')
+    },
+    ...mapGetters(['pageLinks'])
+  }
+}
 </script>
 
 <style scoped>
-footer {
-  text-align: center;
+.MainFooter {
+  color: #333333;
+  width: 100%;
+  margin-top: solid 1px #f1eaea;
+}
+
+.MainFooter > a {
+  font-family: 'Cabin', sans-serif;
+  font-size: 0.75rem;
+  line-height: 0.93rem;
+  text-transform: uppercase;
+  color: #666666;
+}
+
+.MainFooter > a:first-child {
+  margin-top: 1rem;
+}
+
+@media only screen and (min-width: 768px) {
+  .MainFooter {
+    text-align: right;
+  }
 }
 </style>
