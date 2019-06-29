91352489a902d5df652be479de844094fe5e85ac
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-06-29 15:56:05 -0300

    feat: layout novo

---
+++ src/layouts/alt.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="main-wrapper">
+    <header>
+      <fixed-header :fixed.sync="navBarFixed" :threshold="100">
+        <NewNavBar />
+      </fixed-header>
+    </header>
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NewNavBar from '~/components/common/NewNavBar'
+import Footer from '~/components/common/Footer'
+import FixedHeader from 'vue-fixed-header'
+export default {
+  name: 'Alt',
+  components: {
+    NewNavBar,
+    Footer,
+    FixedHeader
+  },
+  data() {
+    return {
+      navBarFixed: false
+    }
+  }
+}
+</script>
