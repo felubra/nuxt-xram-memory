d1810f21f64ea48fac1613742f6a9dc0d86b35bd
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-06-29 15:37:47 -0300

    feat: finalização do componente de rodapé

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,17 +1,22 @@
 <template>
   <footer class="MainFooter">
-    <Logo></Logo>
+    <Logo variant="claro" />
     <p>
-      &copy; 2019 xRAM-Memory.
-      <br>Todos os direitos reservados.
+      &copy; 2019 xraM-Memory.
+      <br />Todos os direitos reservados.
     </p>
-    <nuxt-link v-for="(index, link) in footerLinks" :key="index">{{link.name}}</nuxt-link>
+    <nuxt-link
+      v-for="(page, index) in footerLinks"
+      :key="index"
+      :to="urlOrRoute(page)"
+    >{{page.title}}</nuxt-link>
   </footer>
 </template>
 
 <script>
 import Logo from './Logo'
 import { mapGetters } from 'vuex'
+import { urlOrRoute } from '@/utils'
 export default {
   components: {
     Logo
@@ -21,23 +26,34 @@ export default {
       return this.pageLinks('menu-footer')
     },
     ...mapGetters(['pageLinks'])
+  },
+  methods: {
+    urlOrRoute
   }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 .MainFooter {
   color: #333333;
   width: 100%;
+  border-top: solid 1px #f1eaea;
+  margin-top: auto;
   max-width: $max-width;
+  margin: 0 auto;
+  padding: 1rem 0;
+  text-align: center;
+  font-size: 14px;
 }
 
 .MainFooter > a {
   font-family: 'Cabin', sans-serif;
-  font-size: 0.75rem;
+  font-size: $microtext;
   line-height: 0.93rem;
   text-transform: uppercase;
   color: #666666;
+  display: block;
+  margin: 6px 0;
 }
 
 .MainFooter > a:first-child {
@@ -45,8 +61,14 @@ export default {
 }
 
 @media only screen and (min-width: 768px) {
-  .MainFooter {
-    text-align: right;
+  .MainFooter > a {
+    font-family: 'Cabin', sans-serif;
+    font-size: $microtext;
+    line-height: 0.93rem;
+    text-transform: uppercase;
+    color: #666666;
+    display: inline-block;
+    margin: 0 6px;
   }
 }
 </style>
