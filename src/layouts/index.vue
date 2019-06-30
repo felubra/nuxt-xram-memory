ea3a5ebdf95a986200a7eca1a227916cb29f8ab8
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-06-29 22:54:53 -0300

    feat: layout para página índice

---
+++ src/layouts/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main-wrapper">
+    <header>
+      <NewNavBar :show-logo="false" />
+    </header>
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NewNavBar from '~/components/common/NewNavBar'
+import Footer from '~/components/common/Footer'
+
+export default {
+  name: 'Alt',
+  components: {
+    NewNavBar,
+    Footer
+  }
+}
+</script>
