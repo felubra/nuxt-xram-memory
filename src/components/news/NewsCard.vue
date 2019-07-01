4366011d14d359e30f056b243db0fee9b2055b7f
Felipe Lube de Bragança <felipe.braganca@PRODEST.ES.GOV.BR.LOCAL>
2019-07-01 11:47:51 -0300

    feat: componente NewsGrid para exibir as notícias num estilo masonry

---
+++ src/components/news/NewsCard.vue
@@ -0,0 +1,247 @@
+<template>
+  <div class="NewsCard">
+    <nuxt-link :to="itemLink">
+      <div class="NewsCard__Body">
+        <div class="NewCard__Image">
+          <img v-if="image" :src="image" alt @error="removeImage" />
+        </div>
+        <div class="news-text">
+          <p v-if="showLabel" class="label">{{label}}</p>
+          <h3>{{ resultItem.title }}</h3>
+          <p class="teaser">{{teaser}}</p>
+        </div>
+      </div>
+    </nuxt-link>
+    <div v-if="footer" class="NewsCard__Footer">
+      <p class="NewsCard__Newspaper">
+        <a :href="newspaper.url">
+          <img v-if="newspaperIcon" :src="newspaperIcon" alt />
+          <strong>{{newspaper.title}}</strong>
+        </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+/**TODO: adicionar prop centered */
+const dayJs = require('dayjs')
+const { getMediaUrl, sanitize } = require('~/utils')
+export default {
+  name: 'NewsCard',
+  props: {
+    resultItem: {
+      type: Object,
+      default: function() {
+        return {}
+      }
+    },
+    showLabel: {
+      type: Boolean,
+      default: true
+    },
+    footer: {
+      type: Boolean,
+      default: true
+    },
+    info: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    title() {
+      return sanitize(this.resultItem.title)
+    },
+    teaser() {
+      return sanitize(this.resultItem.teaser)
+    },
+    image() {
+      try {
+        return (
+          (this.resultItem.image_capture &&
+            getMediaUrl(this.resultItem.image_capture)) ||
+          (this.resultItem.thumbnail && getMediaUrl(this.resultItem.thumbnail))
+        )
+      } catch {
+        /** TODO: retorne um ícone padrão se não houver imagem */
+        return ''
+      }
+    },
+    itemLink() {
+      switch (this.type) {
+        case 'Documento': {
+          return {
+            name: 'document-document_id',
+            params: { document_id: this.resultItem.document_id }
+          }
+        }
+        case 'Notícia': {
+          return { name: 'news-slug', params: { slug: this.resultItem.slug } }
+        }
+        default: {
+          return { name: 'search' }
+        }
+      }
+    },
+    type() {
+      return this.resultItem._type || ''
+    },
+    newspaperIcon() {
+      try {
+        return getMediaUrl(this.resultItem.newspaper.favicon_logo)
+      } catch (e) {
+        return ''
+      }
+    },
+    newspaper() {
+      if (
+        this.resultItem.newspaper &&
+        this.resultItem.newspaper.url &&
+        this.resultItem.newspaper.title
+      ) {
+        return {
+          url: this.resultItem.newspaper.url,
+          title: this.resultItem.newspaper.title
+        }
+      }
+      return false
+    },
+    published_date() {
+      try {
+        const dateTime = dayJs(this.resultItem.published_date)
+        if (!dateTime.isValid()) {
+          throw new Error()
+        }
+        return dateTime.toDate().toLocaleDateString()
+      } catch {
+        return ''
+      }
+    },
+    label() {
+      const values = [this.type, this.published_date]
+      return values.filter(value => value).join(' em ')
+    }
+  },
+  methods: {
+    removeImage(e) {
+      e.target.remove()
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+p.label {
+  color: #aa0000;
+  text-transform: uppercase;
+  text-align: center;
+  font-weight: bold;
+  font-size: 0.75rem;
+  font-family: 'Cabin', sans-serif;
+}
+
+.NewsCard a:hover, .NewsCard a:focus {
+  text-decoration: none;
+}
+
+.NewsCard {
+  background: #f3f1f1;
+  padding: 1rem 1rem 0.6rem 1rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  transition: color 0.25s ease, background-color 0.25s ease;
+  color: inherit;
+  text-decoration: none;
+  border: solid 1px #efefef;
+  transition: border 0.25s ease, background-color 0.25s ease;
+  display: inline-block;
+  background: #faf9f6;
+}
+
+.NewsCard h3, .NewsCard p {
+  transition: color 0.25s ease;
+}
+
+.NewsCard p {
+  color: #555;
+}
+
+.NewsCard:hover {
+  border-color: #ce5454;
+}
+
+.NewsCard:hover h3 {
+  color: #ce5454;
+}
+
+.NewsCard:hover p {
+  color: #8c1010;
+}
+
+.NewsCard:focus, .NewsCard:active {
+  background: #ce5454;
+  color: #fff;
+}
+
+.NewsCard:focus a, .NewsCard:active h3, .NewsCard:focus h3, .NewsCard:focus h3, .NewsCard:active p, .NewsCard:focus p {
+  color: #fff;
+  outline-color: #fff;
+}
+
+div.NewsCard__Body {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+div.NewsCard__Body p.teaser {
+  display: none;
+}
+
+div.NewCard__Image {
+  min-width: 15.625rem;
+}
+
+div.NewsCard__Body h3 {
+  font-weight: 500;
+  margin: 0 0 1rem;
+  font-size: 1.25rem;
+  line-height: 1.2;
+}
+
+div.NewsCard__Footer {
+  justify-content: flex-end;
+}
+
+div.NewsCard__Footer a.newspaper {
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #333333;
+}
+
+div.NewsCard__Footer a.newspaper:visited {
+  color: #333333;
+}
+
+p.NewsCard__Newspaper {
+  font-family: Cabin, sans-serif;
+  font-size: 0.75rem;
+  margin: 0;
+  padding: 0;
+}
+
+p.NewsCard__Newspaper a {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+}
+
+p.NewsCard__Newspaper img {
+  margin-right: 0.27rem;
+}
+</style>
---
+++ src/components/news/NewsGrid.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    v-masonry
+    class="masonry-container"
+    transition-duration="0.3s"
+    item-selector=".masonry-container > .item"
+    column-width="270"
+    gutter="32"
+    fit-width="true"
+  >
+    <div v-for="(item, index) in items" :key="index" v-masonry-tile class="item">
+      <NewsCard class="item" :result-item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import NewsCard from './NewsCard'
+
+export default {
+  name: 'NewsGrid',
+  components: {
+    NewsCard
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.item {
+  width: 270px;
+  min-height: 420px;
+  margin: 6px 0;
+}
+
+.masonry-container {
+  margin: 0 auto;
+}
+</style>
