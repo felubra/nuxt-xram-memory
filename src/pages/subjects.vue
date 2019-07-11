17b8bac6b375798088229ae778e11ae343a8dc7a
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-07-11 19:24:33 -0300

    feat: página de assuntos

---
+++ src/pages/subjects.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="SubjectsPage">
+    <section class="SubjectsPage__Featured">
+      <Microtext arrow="down">Assuntos em destaque</Microtext>
+      <div class="SubjectsList">
+        <SubjectCard :subject="leftSubject" />
+        <SubjectCard :subject="featuredSubject" :big="true" />
+      </div>
+    </section>
+    <section class="SubjectsPage__TagCloud">
+      <Microtext arrow="down">Nuvem de palavras-chave</Microtext>
+      <ReactiveBase
+        class-name="PageIndex"
+        app="artifact_document,artifact_news"
+        :url="reactiveServerURL"
+        :theme="reactiveDefaultTheme"
+        :credentials="reactiveCredentials"
+      >
+        <HomeTagCloud />
+      </ReactiveBase>
+    </section>
+  </section>
+</template>
+
+<script>
+import Microtext from '~/components/common/Microtext'
+import SubjectCard from '~/components/SubjectCard'
+import HomeTagCloud from '~/components/home/HomeTagCloud'
+
+import reactiveMixin from '~/utils/reactiveMixin'
+
+export default {
+  layout: 'alt',
+  components: {
+    Microtext,
+    SubjectCard,
+    HomeTagCloud
+  },
+  mixins: [reactiveMixin],
+  data() {
+    return {
+      subjects: []
+    }
+  },
+  computed: {
+    leftSubject() {
+      return this.subjects[0]
+    },
+    featuredSubject() {
+      return this.subjects[1]
+    }
+  },
+  async asyncData({ $axios, route, error }) {
+    const subjects = await $axios.$get(`api/v1/subjects/page`)
+    return {
+      subjects
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.SubjectsList {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+}
+
+.SubjectsList > .SubjectCard {
+  margin-right: 1rem;
+}
+
+.SubjectsPage__Featured > .SubjectsList > .SubjectCard:last-child {
+  flex-grow: 1;
+  margin-right: 0;
+}
+
+.SubjectsPage__TagCloud, .SubjectsPage__Featured {
+  max-width: $max-width;
+  margin: 4rem auto;
+}
+</style>
