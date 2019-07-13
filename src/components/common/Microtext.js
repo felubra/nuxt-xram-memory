fe03821e7d3dd66d63f21f53f2dd6131c0096998
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-07-13 10:48:19 -0300

    feat: componente Microtext em js puro, com suporte a tag customizada

---
+++ src/components/common/Microtext.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+export default Vue.component('Microtext', {
+  functional: true,
+  props: {
+    tag: {
+      type: String,
+      default: () => 'p',
+      validator(value) {
+        return ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span'].includes(value)
+      }
+    },
+    arrow: {
+      type: String,
+      validator(value) {
+        return ['right', 'up', 'down', 'left', 'none'].includes(value)
+      },
+      default: 'none'
+    }
+  },
+  render(createElement, context) {
+    const { arrow } = context.props
+    if (arrow !== 'none') {
+      const arrowIcon =
+        arrow === 'down' || arrow === 'up'
+          ? `arrow_drop_${arrow}`
+          : `arrow_${arrow}`
+      const arrowElement = createElement(
+        'i',
+        { class: ['material-icons'] },
+        arrowIcon
+      )
+      return createElement(
+        context.props.tag,
+        { class: ['microtext', `microtext--arrow-${arrow}`] },
+        [context.slots().default, arrowElement]
+      )
+    }
+    return createElement(
+      context.props.tag,
+      { class: ['Microtext'] },
+      context.slots().default
+    )
+  }
+})
