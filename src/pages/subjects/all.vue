5f349eb1cf4530e782490af5a55f22aae9aec3b7
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-07-22 10:46:21 -0300

    feat: (wip): página de todos os assuntos

---
+++ src/pages/subjects/all.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="Page CenteredPage">
+    <h1 class="off-screen">Todos os assuntos</h1>
+    <header>
+      <Microtext arrow="down">Assuntos (em ordem alfabética)</Microtext>
+      <AlphabeticalPicker />
+    </header>
+    <main></main>
+  </section>
+</template>
+
+<script>
+import Microtext from '~/components/common/Microtext'
+
+export default {
+  name: 'AllSubjectsPage',
+  components: {
+    Microtext
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
