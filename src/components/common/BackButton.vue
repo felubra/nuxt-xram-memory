c9f0f561095c2c2fdd3251ed70a73876ccc8cf5e
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-07-26 10:46:21 -0300

    feat: pequeno componente para botão voltar

---
+++ src/components/common/BackButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-button icon="el-icon-back" circle @click="goBack()"></el-button>
+</template>
+
+<script>
+export default {
+  name: 'BackButton',
+  methods: {
+    goBack() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
