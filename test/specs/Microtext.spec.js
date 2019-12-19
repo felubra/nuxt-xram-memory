ffdb944985f902ef92f10ea54ea4a30972c39646
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-12-19 18:30:14 -0300

    feat: testes para o componente Microtext

--- test/specs/Microtext.spec.js
+++ test/specs/Microtext.spec.js
@@ -2,7 +2,7 @@ import { mount, shallowMount } from '@vue/test-utils'
 import Microtext from '@/components/common/Microtext.js'
 
 describe('Microtext.js', () => {
-  it('deve renderizar como um elemento <p> por padrão', () => {
+  it('deve renderizar como um elemento <p> e classe padrão', () => {
     const text = 'este é um texto'
     const wrapper = shallowMount(Microtext, {
       slots: {
@@ -10,5 +10,86 @@ describe('Microtext.js', () => {
       }
     })
     expect(wrapper.find('p').text()).toMatch(text)
+    expect(wrapper.classes('microtext')).toBe(true)
+  })
+
+  it('deve usar uma tag customizada', () => {
+    const text = 'este é um texto'
+    const wrapper = shallowMount(Microtext, {
+      slots: {
+        default: text
+      },
+      propsData: {
+        tag: 'h1'
+      }
+    })
+    expect(wrapper.find('h1').text()).toMatch(text)
+  })
+
+  it('deve aceitar uma classe adicional', () => {
+    const text = 'este é um texto'
+    const wrapper = shallowMount(Microtext, {
+      slots: {
+        default: text
+      },
+      propsData: {
+        className: 'abacate'
+      }
+    })
+    expect(wrapper.classes('microtext')).toBe(true)
+    expect(wrapper.classes('abacate')).toBe(true)
+  })
+
+  it('deve renderizar um ícone de seta', () => {
+    const text = 'este é um texto'
+    const wrapper = shallowMount(Microtext, {
+      slots: {
+        default: text
+      },
+      propsData: {
+        tag: 'h1',
+        arrow: 'left'
+      }
+    })
+    expect(wrapper.find('h1').text()).toMatch(text)
+    expect(wrapper.find('h1').contains('i')).toBe(true)
+    expect(
+      wrapper
+        .find('h1')
+        .find('i')
+        .classes('material-icons')
+    ).toBe(true)
+    expect(
+      wrapper
+        .find('h1')
+        .find('i')
+        .text()
+    ).toBe('arrow_left')
+    expect(wrapper.find('h1').classes('microtext--arrow-left')).toBe(true)
+
+    const wrapper2 = shallowMount(Microtext, {
+      slots: {
+        default: text
+      },
+      propsData: {
+        tag: 'h1',
+        arrow: 'down'
+      }
+    })
+
+    expect(wrapper2.find('h1').classes('microtext--arrow-down')).toBe(true)
+    expect(
+      wrapper2
+        .find('h1')
+        .find('i')
+        .classes('material-icons')
+    ).toBe(true)
+
+    expect(
+      wrapper2
+        .find('h1')
+        .find('i')
+        .text()
+    ).toBe('arrow_drop_down')
   })
 })
