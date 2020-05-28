339a1b9c41dbd8adfada2d2c48f722a153b6924f
Felipe <felipe.lubra@gmail.com>
2020-05-28 09:28:04 -0300

    feat: LocalSearchBase: estado e funções para alterar a busca e os filtros

---
+++ src/components/search/LocalSearchBase.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'LocalSearchBase',
+  data() {
+    return {
+      //TODO: indexState: EMPTY,
+      //TODO: results: [],
+      searchState: {},
+      filterState: {}
+    }
+  },
+  methods: {
+    searchBy(componentId, value) {
+      if (value) {
+        this.$set(this.searchState, componentId, value)
+      } else {
+        this.$delete(this.searchState, componentId)
+      }
+    },
+    filterBy(componentId, value) {
+      if (value) {
+        this.$set(this.filterState, componentId, value)
+      } else {
+        this.$delete(this.filterState, componentId)
+      }
+    }
+  },
+  provide() {
+    return {
+      searchBy: this.searchBy,
+      filterBy: this.filterBy
+    }
+  },
+  render(h) {
+    return h('div', [
+      this.$scopedSlots.default({
+        text: this.message
+      })
+    ])
+  }
+}
+</script>
+
+<style>
+</style>
