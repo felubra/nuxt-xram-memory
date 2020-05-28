26b1b375a05260619b904ddfd0d335e968df3936
Felipe <felipe.lubra@gmail.com>
2020-05-28 09:28:43 -0300

    chore: página de testes para novos componentes

---
+++ src/pages/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <LocalSearchBase>
+      <LocalSearchInput component-id="mainQuery" />
+      <LocalSearchDropDown component-id="mainFilter" />
+    </LocalSearchBase>
+  </div>
+</template>
+
+<script>
+import LocalSearchBase from '@/components/search/LocalSearchBase'
+import LocalSearchInput from '@/components/search/LocalSearchInput'
+import LocalSearchDropDown from '@/components/search/LocalSearchDropDown'
+
+export default {
+  name: 'TestPage',
+  components: {
+    LocalSearchBase,
+    LocalSearchInput,
+    LocalSearchDropDown
+  }
+}
+</script>
+
+<style>
+</style>
