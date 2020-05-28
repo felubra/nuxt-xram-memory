82080ca4693289d8cba9aacb4d8dcdc8692eb4dd
Felipe <felipe.lubra@gmail.com>
2020-05-28 09:28:33 -0300

    feat: LocalSearchDropDown e LocalSearchInput: manipulação básica de estado

---
+++ src/components/search/LocalSearchDropDown.vue
@@ -0,0 +1,37 @@
+<template>
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'LocalSearchDropDown',
+  props: {
+    componentId: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selected: ''
+    }
+  },
+  watch: {
+    selected: {
+      immediate: true,
+      handler(v) {
+        this.filterBy(this.componentId, v)
+      }
+    }
+  },
+  inject: ['filterBy']
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/search/LocalSearchInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" @change="(e) => searchBy(componentId, e.target.value)" />
+</template>
+
+<script>
+export default {
+  name: 'LocalSearchInput',
+  props: {
+    componentId: {
+      type: String,
+      required: true
+    }
+  },
+  inject: ['searchBy']
+}
+</script>
+
+<style>
+</style>
