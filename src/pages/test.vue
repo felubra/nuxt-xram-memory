c47350015fa2557f91db3db6c39e76639a17e5cb
Felipe <felipe.lubra@gmail.com>
2020-05-31 17:23:40 -0300

    feat: adicione mais filtros e uma lista com os resultados

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <LocalSearchBase index-u-r-l="media/lunr_index/index.json">
+    <LocalSearchBase v-slot:default="{results}" index-u-r-l="media/lunr_index/index.json">
       <LocalSearchInput component-id="mainQuery" />
+      <LocalSearchDropDown field-name="type" component-id="typeFilter" />
       <LocalSearchDropDown field-name="keywords" component-id="keywordsFilter" />
       <LocalSearchDropDown field-name="subjects" component-id="subjectsFilter" />
       <LocalSearchDropDown field-name="newspaper.title" component-id="newspaperFilter" />
+      <NewsGrid :items='results' />
     </LocalSearchBase>
   </div>
 </template>
@@ -13,13 +15,15 @@
 import LocalSearchBase from '@/components/search/LocalSearchBase'
 import LocalSearchInput from '@/components/search/LocalSearchInput'
 import LocalSearchDropDown from '@/components/search/LocalSearchDropDown'
+import NewsGrid from '@/components/news/NewsGrid'
 
 export default {
   name: 'TestPage',
   components: {
     LocalSearchBase,
     LocalSearchInput,
-    LocalSearchDropDown
+    LocalSearchDropDown,
+    NewsGrid
   }
 }
 </script>
