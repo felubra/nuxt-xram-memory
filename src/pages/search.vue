892fb3c3e71b7905784e1e74b4671dbf24c57f40
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-03 18:09:15 -0300

    chore: substitua a página de busca pela nova desenvolvida

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -1,43 +1,121 @@
 <template>
-  <client-only>
-    <DefaultReactiveBase class-name="Page SearchPage">
-      <h1 class="offscreen">Pesquisar</h1>
-      <!-- eslint-disable -->
-      <HomeSearchInput class="SearchBar--as-filter" :URLParams="true" />
-      <!-- eslint-enable -->
-      <SearchFilters />
-      <HomeSearchResults />
-    </DefaultReactiveBase>
-  </client-only>
+  <LocalSearchBase
+    v-slot:default="{searchResults, resultCount, clear, isLoading, hasError}"
+    class="TestPage"
+    :initial-state="initialState"
+    index-u-r-l="media/lunr_index/index.json">
+    <transition appear name="fade" mode="out-in">
+      <div v-if="hasError" key="error" class="Error" >
+        Desculpe-nos, mas um erro aconteceu. Tente recarregar esta página mais tarde.
+      </div>
+      <div v-else key="loaded" >
+        <div>
+          <div class="SearchBar">
+            <LocalSearchInput component-id="mainQuery" placeholder="Pesquisar no acervo" />
+          </div>
+          <div class="Filters">
+            <div>
+              <label for="typeFilter"><Microtext>Tipo</Microtext></label>
+              <LocalSearchDropDown field-name="type" component-id="typeFilter" />
+            </div>
+            <div>
+              <label for="siteFilter"><Microtext>Site/Veículo</Microtext></label>
+              <LocalSearchDropDown field-name="newspaper.title" component-id="newspaperFilter" />
+            </div>
+            <div>
+              <label for="keywordsFilter"><Microtext>Palavras-chave</Microtext></label>
+              <LocalSearchDropDown field-name="keywords" component-id="keywordsFilter" />
+            </div>
+            <div>
+              <label for="subjectsFilter"><Microtext>Assuntos</Microtext></label>
+              <LocalSearchDropDown field-name="subjects" component-id="subjectsFilter" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <ResultStats :total-results="resultCount" />
+        </div>
+        <NewsGrid
+          v-loading="isLoading"
+          class="NewsGrid"
+          :items='searchResults'
+          element-loading-text="Carregando..."
+          element-loading-background="transparent"
+        />
+      </div>
+    </transition>
+  </LocalSearchBase>
 </template>
 
 <script>
-import HomeSearchResults from '~/components/home/HomeSearchResults'
-import DefaultReactiveBase from '@/components/DefaultReactiveBase'
-import HomeSearchInput from '~/components/home/HomeSearchInput'
-import SearchFilters from '@/components/home/SearchFilters'
+import LocalSearchBase from '@/components/search/LocalSearchBase'
+import LocalSearchInput from '@/components/search/LocalSearchInput'
+import LocalSearchDropDown from '@/components/search/LocalSearchDropDown'
+import NewsGrid from '@/components/news/NewsGrid'
+import ResultStats from '@/components/home/ResultStats.vue'
 
 export default {
-  name: 'SearchPage',
+  name: 'TestPage',
   components: {
-    HomeSearchInput,
-    DefaultReactiveBase,
-    SearchFilters,
-    HomeSearchResults
+    LocalSearchBase,
+    LocalSearchInput,
+    LocalSearchDropDown,
+    NewsGrid,
+    ResultStats
   },
-  head: {
-    title: 'xraM-Memory - Pesquisar'
+  data() {
+    return {
+      initialState: {}
+    }
+  },
+  asyncData({ route }) {
+    return {
+      initialState: {
+        filterState: route.query
+      }
+    }
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-.SearchPage {
-  margin-top: 2rem;
+<style scoped>
+.TestPage {
+  flex: 1;
+  display: flex;
+  position: relative;
+}
+
+.TestPage > div {
+  min-height: 10vh;
+  width: 100%;
+}
+.Filters {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 4rem 0 2rem;
+}
+.Filters > div {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 0 2rem;
 }
 
 .SearchBar {
-  margin-left: auto;
-  margin-right: auto;
+  max-width: 53rem;
+  margin: 3rem auto 0;
+}
+
+@media only screen and (min-width: 768px) {
+  .Filters {
+    flex-direction: row;
+  }
+  .Filters > div {
+    display: flex;
+    flex-direction: column;
+    width: auto;
+  }
 }
 </style>
