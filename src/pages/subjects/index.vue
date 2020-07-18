d2e1a1b547fd2c20389b89e48dca598014b4af69
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-18 17:39:56 -0300

    fix: correção de tipo de initialSubjects

--- src/pages/subjects/index.vue
+++ src/pages/subjects/index.vue
@@ -83,7 +83,7 @@ export default {
           `api/v1/subjects/initial/${initialSelectedInitial}`
         )
       } else {
-        initialSubjects = ''
+        initialSubjects = []
       }
     } catch {
       subjectInitials = []
