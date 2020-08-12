a5cf8580ea536acc00f05b463de34890b42f6f67
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-08-12 19:31:07 -0300

    feat: suporte à pesquisa com e sem acentos

--- src/components/search/lunr.worker.js
+++ src/components/search/lunr.worker.js
@@ -1,5 +1,10 @@
 import * as Comlink from 'comlink'
 import lunr from 'elasticlunr'
+require('lunr-languages/lunr.stemmer.support')(lunr)
+require('lunr-languages/lunr.pt')(lunr)
+const asciiFolder = require('fold-to-ascii')
+const replaceDiacritics = token => asciiFolder.foldReplacing(token)
+lunr.Pipeline.registerFunction(replaceDiacritics, 'replaceDiacritics')
 
 const obj = {
   index: null,
