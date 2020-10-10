2f1c03b3d48b969f08fa56f9e26ca327b2051dd6
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-10-10 18:09:56 -0300

    Revert "chore: atualização (das configurações) do eslint"
    This reverts commit 3375f5d1aafdbc42c66ddbce78a10dba51b3e253.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,11 @@ module.exports = {
     browser: true,
     node: true
   },
-  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: ['plugin:vue/recommended', 'standard'],
+  extends: ['plugin:vue/essential', 'plugin:vue/recommended', 'standard'],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {
-    'vue/no-unused-properties': ['error', {
-      groups: ['props', 'data', 'computed', 'methods']
-    }],
-    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
-      registeredComponentsOnly: true,
-      ignores: []
-    }],
-    'vue/html-comment-indent': ['error', 2]
-  }
+  rules: {}
 }
