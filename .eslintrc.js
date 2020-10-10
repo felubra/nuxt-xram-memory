3375f5d1aafdbc42c66ddbce78a10dba51b3e253
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-10-10 16:44:32 -0300

    chore: atualização (das configurações) do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,21 @@ module.exports = {
     browser: true,
     node: true
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: ['plugin:vue/essential', 'plugin:vue/recommended', 'standard'],
+  extends: ['plugin:vue/recommended', 'standard'],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/no-unused-properties': ['error', {
+      groups: ['props', 'data', 'computed', 'methods']
+    }],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+      registeredComponentsOnly: true,
+      ignores: []
+    }],
+    'vue/html-comment-indent': ['error', 2]
+  }
 }
