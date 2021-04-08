e30b7037f38351fb251198e50fa4007eb25fe80a
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2021-04-08 20:10:09 -0300

    feat: create a plugin to load the fonts

---
+++ src/plugins/custom/fontSource.js
@@ -0,0 +1,14 @@
+import '@fontsource/material-icons'
+import '@fontsource/material-icons-outlined'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/400-italic.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/500-italic.css'
+
+import '@fontsource/vollkorn/400.css'
+import '@fontsource/vollkorn/400-italic.css'
+import '@fontsource/vollkorn/700.css'
+import '@fontsource/vollkorn/700-italic.css'
+
+import '@fontsource/vollkorn-sc'
